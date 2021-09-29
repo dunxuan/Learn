24a78b9701a06af6313c0a587f2b57afc5b6f9a2
顿玄 <m20020388@126.com>
2021-09-29 11:41:26 +0800

    解一元二次方程或一元一次方程

--- Learn/CPP/code.cpp
+++ Learn/CPP/code.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    float a = 0;
+	float a = 0;
     float b = 0;
     float c = 0;
     float x1 = 0;
@@ -15,19 +15,17 @@ int main()
     char confirm = 0;
 
     for (;;)    {
-        cout << "求解ax²+bx+c=0, 输入求解一元二次函数 (q) 或一元一次函数 (l) ";
-        cout << endl;
+        cout << "求解ax²+bx+c=0, 输入求解一元二次方程 (q) / 一元一次方程 (l):";
         cin >> mod;
         if (mod == 'q' || mod == 'Q') {
-            //一元二次函数
+            //一元二次方程
             //输入过程
             do  {
                 //输入abc中
                 cout << "a=";
                 cin >> a;
-                cout << endl;
                 while (!a)  {
-                    cout << "a不能等于0，切换到一元一次函数 (l) 或 重新输入 (r)";
+                    cout << "a不能等于0，切换到一元一次方程 (l) 重新输入 (r)";
                     cout << endl;
                     cin >> confirm;
                     if (confirm == 'l' || confirm == 'L')   {
@@ -35,15 +33,12 @@ int main()
                     } else if (confirm == 'r' || confirm == 'R')    {
                         cout << "a=";
                         cin >> a;
-                        cout << endl;
                     }
                 }
                 cout << "b=";
                 cin >> b;
-                cout << endl;
                 cout << "c=";
                 cin >> c;
-                cout << endl;
                 //确认过程
                 cout << "解";
                 cout << a;
@@ -52,44 +47,50 @@ int main()
                 cout << "x+";
                 cout << c;
                 cout << "=0";
-                cout << endl;
+				cout << endl;
                 do {
-                    cout << "确认(Y) 取消并重新输入(n)";
-                    cout << endl;
+                    cout << "确认(Y) 取消并重新输入(n):";
                     cin >> confirm;
-                } while (confirm != 'Y' || confirm != 'y' || confirm != 'N' || confirm != 'n');
+                } while (confirm != 'Y' && confirm != 'y' && confirm != 'N' && confirm != 'n');
             } while (confirm =='N' || confirm == 'n');
 
             //运算过程
             delta = b * b - 4 * a * c;
-            x1 = (-b - sqrt(delta) / (2 * a));
-            x2 = (-b + sqrt(delta) / (2 * a));
+			if (delta > 0)	{
+				x1 = (-b - sqrt(delta) / (2 * a));
+            	x2 = (-b + sqrt(delta) / (2 * a));
 
-            //输出结果
-            cout << "x₁=";
-            cout << x1;
-            cout << endl;
-            cout << "x₂=";
-            cout << x2;
-            cout << endl;
+				//输出结果
+            	cout << "x₁=";
+            	cout << x1;
+				cout << endl;
+            	cout << "x₂=";
+            	cout << x2;
+				cout << endl;
+			} else if (!delta)	{
+				x1 = - b / 2 / a;
+				
+				cout << "x₁=x₂=";
+				cout << x1;
+				cout << endl;
+			}
+
         } else if (mod == 'l' || mod == 'L')    {
-            //一元一次函数
+            //一元一次方程
             LINEAR:             //goto跳转
             //输入过程
             do {
-                cout << "b=";
-                cin >> b;
-                cout << endl;
-                while (!b)  {
+            	cout << "b=";
+            	cin >> b;
+            	cout << endl;
+            	while (!b)  {
                     cout << "b不能等于0，请重新输入";
                     cout << endl;
                     cout << "b=";
                     cin >> b;
-                    cout << endl;
                 }
                 cout << "c=";
                 cin >> c;
-                cout << endl;
                 //确认过程
                 cout << "解";
                 cout << b;
@@ -98,10 +99,9 @@ int main()
                 cout << "=0";
                 cout << endl;
                 do {
-                    cout << "确认(Y)     取消并重新输入(n)";
-                    cout << endl;
+                    cout << "确认(Y) 取消并重新输入(n):";
                     cin >> confirm;
-                } while (confirm != 'Y' || confirm != 'y' || confirm != 'N' || confirm != 'n');
+                } while (confirm != 'Y' && confirm != 'y' && confirm != 'N' && confirm != 'n');
             } while (confirm == 'n' || confirm == 'N');
 
             //运算过程
